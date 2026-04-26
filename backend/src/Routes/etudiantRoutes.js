e85319d4c16c39c1281f84aa93b8b13735648fe6
ELHOUTI Mohamed Amine <mohamedytreza465@gmail.com>
2026-04-26 14:39:10 +0100

    feature:ajout api profil etudiant

---
+++ backend/src/Routes/etudiantRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { 
+    obtenirTousLesProfils, 
+    obtenirProfilParId, 
+    traiterProfil,
+} from '../Controllers/etudiantController.js';
+
+const routeur = express.Router();
+
+routeur.get('/', obtenirTousLesProfils);
+routeur.get('/:id', obtenirProfilParId);
+routeur.put('/:id', traiterProfil);
+
+export default routeur;
