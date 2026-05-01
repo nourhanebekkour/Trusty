fc2c8de839705e2c296ae7f2e0960f82ac8cd2eb
raguiguemalak <malakraguigue@gmail.com>
2026-05-01 09:14:36 +0100

    mise à jour du serveur

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,6 @@ app.use('/api/auth', authRoutes);
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Serveur lancé sur le port ${PORT}`);
 });
