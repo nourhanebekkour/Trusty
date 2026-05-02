2f1f2a22fff19332802670349765ac4e6f6abcba
ELHOUTI Mohamed Amine <mohamedytreza465@gmail.com>
2026-05-02 13:46:59 +0100

    ajout feature CRUD technologie

--- backend/src/Routes/etudiantRoutes.js
+++ backend/src/Routes/etudiantRoutes.js
@@ -4,18 +4,13 @@ import {
     obtenirProfilParId, 
     traiterProfil,
 } from '../Controllers/etudiantController.js';
-import { authMiddleware } from '../Middlewars/auth.middleware.js';
 
 const router = express.Router();
 
-router.use(authMiddleware);
 
-// Tout le monde peut voir les profils (selon visibilité, mais ici on protège au moins la modif)
 router.get('/',obtenirTousLesProfils);
 router.get('/:id',obtenirProfilParId);
 
-// Seul l'utilisateur connecté peut modifier son profil (ou un admin)
-// Pour l'instant on protège juste avec authMiddleware
 router.put('/:id',traiterProfil);
 
 export default router;
--- backend/src/Utils/responseHandler.js
+++ backend/src/Utils/responseHandler.js
@@ -1,11 +1,13 @@
 /**
  * @param {Object} res 
  * @param {number} statusCode 
+ * @param {boolean} success
  * @param {string} message
  */
-const sendResponse = (res, statusCode, message, data = null, error = null) => {
+const sendResponse = (res, statusCode, success, message, data = null, error = null) => {
     return res.status(statusCode).json({
         status: statusCode,
+        success: success,
         message: message,
         data: data,
         erreur: error
