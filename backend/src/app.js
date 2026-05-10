046d8cf18e024e97dd1b8f5cab95fd90e694a0a7
raguiguemalak <malakraguigue@gmail.com>
2026-05-10 16:11:19 +0100

    feat: configuration CORS avec Authorization et déplacement auth.validator.js dans src

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,19 @@ import express from 'express';
 import apiRouter from './Routes/index.js';
 import swaggerUi from 'swagger-ui-express';
 import { createRequire } from 'module';
+import cors from 'cors';
 
 const require = createRequire(import.meta.url);
 const swaggerDocument = require('./swagger-output.json');
 
 const app = express();
 
-// Middlewares de base
-app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}));
+app.use(express.json({ limit: '10kb' }));
 
 // Point d'entrée unique pour toutes les routes de l'API
 app.use('/api', apiRouter);
