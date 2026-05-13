fe2dbf947157f1f90a96918d3a9c7cbda6e3bad5
NihalAloui <aloui.nihal@etu.uae.ac.ma>
2026-05-13 12:55:26 +0100

    settup home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <Navbar />
+    <main>
+      <Hero />
+      <Stats />
+      <Features />
+      <CTA />
+    </main>
+  <Footer />
 </template>
+
+<script>
+import Hero from '@/components/Hero.vue'
+import Stats from '@/components/Stats.vue'
+import Features from '@/components/Features.vue'
+import CTA from '@/components/CTA.vue'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Hero,
+    Stats,
+    Features,
+    CTA,
+    Navbar,
+    Footer
+  }
+}
+</script>
