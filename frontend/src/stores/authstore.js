349062e01c65b29bd75a3c504b1968ef8f0cf611
Younes <younes.elamraoui2040@gmail.com>
2026-05-18 21:50:12 +0100

    correction des erreurs

--- frontend/src/stores/authstore.js
+++ frontend/src/stores/authstore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { authService } from '../services/auth.service'
+import { authService } from '../services/auth.service.jgrs'
 import api from '../api'
 
 export const useAuthStore = defineStore('auth', {
--- frontend/src/views/registerview.vue
+++ frontend/src/views/registerview.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
+import { useAuthStore } from '../stores/authstore'
 
 const router = useRouter()
 const authStore = useAuthStore()
